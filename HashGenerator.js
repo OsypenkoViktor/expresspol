a8ab819553059f219afbfbf8834eb4b25b2899bf
OsypenkoViktor <143928700+OsypenkoViktor@users.noreply.github.com>
2024-05-16 00:04:48 +0300

    Update HashGenerator.js

--- HashGenerator.js
+++ HashGenerator.js
@@ -1,16 +1,14 @@
+//site admin data created manually with this helper - only one admin required
 const bcrypt = require("bcrypt");
-const saltRounds = 10; // Количество "соли", используемое для усиления хеширования
+const saltRounds = 10; 
 
-// Пароль, который вы хотите хешировать
 const plainPassword = "gCare42Nfgh@p";
 
-// Генерация соли и хеширование пароля
+
 bcrypt.hash(plainPassword, saltRounds, (err, hash) => {
   if (err) {
-    // Обработка ошибки
     console.error("Error hashing password:", err);
   } else {
-    // Ваш хеш пароля
     console.log("Hashed password:", hash);
   }
 });
